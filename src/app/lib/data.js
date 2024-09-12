7890ab31e72b4448be4fb1e54cb9b2bff6a77ffb
Sean Connaughton <smc56462@gmail.com>
2024-09-12 10:23:16 -0400

    Revenue Chart update

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -5,12 +5,14 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
+  
+
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    //console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql`SELECT * FROM revenue`;
 
