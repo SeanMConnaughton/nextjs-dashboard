0b5371505b758900ed54bdd3187e17e828ee5e14
Sean Connaughton <smc56462@gmail.com>
2024-09-23 17:30:58 -0400

    signout function

--- auth.config.js
+++ auth.config.js
@@ -3,17 +3,17 @@ export const authConfig = {
       signIn: '/login',
     },
     callbacks: {
-        authorized({ auth, request: { nextUrl } }) {
-          const isLoggedIn = !!auth?.user;
-          const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-          if (isOnDashboard) {
-            if (isLoggedIn) return true;
-            return false; // Redirect unauthenticated users to the login page
-          } else if (isLoggedIn) {
-            return Response.redirect(new URL('/dashboard', nextUrl));
-          }
-          return true;
-        },
+      authorized({ auth, request: { nextUrl } }) {
+        const isLoggedIn = !!auth?.user;
+        const isOnDashboard = nextUrl.pathname.startsWith('/ui');
+        if (isOnDashboard) {
+          if (isLoggedIn) return true;
+          return false; // Redirect unauthenticated users to the login page
+        } else if (isLoggedIn) {
+          return Response.redirect(new URL('/ui/dashboard', nextUrl));
+        }
+        return true;
       },
-      providers: [], // Add providers with an empty array for now
-    };
+    },
+    providers: [], // Add providers with an empty array for now
+  };
--- src/app/ui/login-form.jsx
+++ src/app/ui/login-form.jsx
@@ -1,23 +1,22 @@
 'use client';
 
-import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
-  FingerPrintIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '@/app/ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '../lib/actions';
+import { authenticate } from '@/app/lib/actions';
+
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
   return (
-    <form action = {dispatch} className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+        <h1 className={`mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
@@ -30,14 +29,16 @@ export default function LoginForm() {
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-
+[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2
+h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -49,7 +50,8 @@ export default function LoginForm() {
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer block w-full rounded-md border border-gray-200 py-
+[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
